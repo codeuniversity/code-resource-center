2bc6afaa72affc5d5ca1b370dc282932d618a344
Daniele de Araujo dos Santos <danisantos@Danieles-MacBook-Pro-2.local>
2018-11-15 13:24:23 +0100

    add database info

--- code_resource_center/settings.py
+++ code_resource_center/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'code_resource_center.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crc_db',
+        'USER': 'crcuser',
+        'PASSWORD': 'myresources',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
