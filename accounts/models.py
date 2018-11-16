8102cbb79325ec7c7092fe8c119a3213761c759c
Marie Klaus <marie.klaus@code.berlin>
2018-11-16 15:53:21 +0100

    Add Institution model.

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Account(models.Model):
-    # placeholder attribute:
+    # placeholder attribute!!!
     first_name = models.CharField(max_length=32)
 
 class UserType(models.Model):
-    #placeholder atributes
     type_name = modelsCharField(max_length=32)
+
+class Institution(models.Model):
+    institution_name = modelsCharField(max_length=32)
