f7fa89252749988a49c47aea4e345f43b5465ddd
[Gabriela] <gabriela.sikora@code.berlin>
2018-11-16 15:54:11 +0100

    Create and connect create.html

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+###In the case that django controls the login
+# from django.contrib.auth.decorators import login_required
+
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
+
+# @login_required
+def create(request):
+    return render(request, 'dashboard/create.html')
