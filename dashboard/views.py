d26535599271b54f2fda4935537acb627e362d63
[Gabriela] <gabriela.sikora@code.berlin>
2018-11-19 17:56:42 +0100

    Resolve dashboard/view merge conflict

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,17 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import auth
 
-<<<<<<< HEAD
-###In the case that django controls the login
-# from django.contrib.auth.decorators import login_required
-
-def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
-
-# @login_required
-def create(request):
-    return render(request, 'dashboard/create.html')
-=======
 @login_required(login_url="/accounts/login")
 def dashboard(request):
     user = User.objects;
@@ -21,4 +10,3 @@ def dashboard(request):
         return render(request, 'dashboard/dashboard.html')
     else:
         return redirect('login')
->>>>>>> aa330c016b783796118c6c0710627cdbd965b560
