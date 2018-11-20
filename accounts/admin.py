772ce7442ef1e076a239d4acba8c9dc116a0b471
Marie Klaus <marie.klaus@code.berlin>
2018-11-20 19:50:10 +0100

    correction of UserProfile and Department model.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Department, Institution, UserType
+from .models import Department, Institution, UserType, UserRole, UserProfile
 
 admin.site.register(Department)
 admin.site.register(Institution)
 admin.site.register(UserType)
+admin.site.register(UserRole)
+admin.site.register(UserProfile)
--- accounts/models.py
+++ accounts/models.py
@@ -17,7 +17,7 @@ class Department(models.Model):
     department_name = models.CharField(max_length=32)
 
     def __str__(self):
-        return self.institution_name
+        return self.department_name
 
 class UserRole(models.Model):
     role_name = models.CharField(max_length=32)
@@ -27,7 +27,8 @@ class UserRole(models.Model):
 
 class UserProfile(models.Model):
     django_user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_type_id = models.ForeignKey(UserType, on_delete=models.SET_DEFAULT)
-    institution_id = models.ForeignKey(Institution, default=2, on_delete=models.SET_DEFAULT)
-    role_id = models.ForeignKey(UserRole, default=2, on_delete=models.SET_DEFAULT)
+    user_type_id = models.ForeignKey(UserType, default=6, on_delete=models.SET_DEFAULT)
+    institution_id = models.ForeignKey(Institution, default=1, on_delete=models.SET_DEFAULT)
+    department_id = models.ForeignKey(Department, default=5, on_delete=models.SET_DEFAULT)
+    role_id = models.ForeignKey(UserRole, default=1, on_delete=models.SET_DEFAULT)
     image = models.ImageField(upload_to='profile_images', blank=True)
