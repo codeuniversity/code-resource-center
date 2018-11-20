647dec763c6f4646191ea826bf39b3c1844c6099
Marie Klaus <marie.klaus@code.berlin>
2018-11-20 18:29:07 +0100

    Install pillow. Create UserType model and UserProfile model.

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,11 @@
 from django.db import models
-
-# class CrcUser(models.Model):
-    # placeholder attribute!!!
-    # first_name = models.CharField(max_length=32)
+from django.contrib.auth.models import User
 
 class UserType(models.Model):
     type_name = models.CharField(max_length=32)
 
     def __str__(self):
         return self.type_name
-
 
 class Institution(models.Model):
     institution_name = models.CharField(max_length=32)
@@ -21,4 +17,17 @@ class Department(models.Model):
     department_name = models.CharField(max_length=32)
 
     def __str__(self):
-        return self.department_name
+        return self.institution_name
+
+class UserRole(models.Model):
+    role_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.role_name
+
+class UserProfile(models.Model):
+    django_user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type_id = models.ForeignKey(UserType, on_delete=models.SET_DEFAULT)
+    institution_id = models.ForeignKey(Institution, default=2, on_delete=models.SET_DEFAULT)
+    role_id = models.ForeignKey(UserRole, default=2, on_delete=models.SET_DEFAULT)
+    avatar = models.ImageField(upload_to='images')
