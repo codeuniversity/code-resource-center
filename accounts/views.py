b40d11c337f8f034be36ba8e9f20f8d10d04e514
Marie Klaus <marie.klaus@code.berlin>
2018-11-20 21:11:40 +0100

    Dynamically generate 'Your role at Code?' selection.

--- accounts/views.py
+++ accounts/views.py
@@ -6,19 +6,25 @@ from django.contrib.auth.decorators import login_required
 
 def signup(request):
     if request.method == 'POST':
+        # check if passwords match
         if request.POST['password'] == request.POST['passwordrepeat']:
             try:
+                # convert email into username
+                # name = 
                 User.objects.get(username=request.POST['username'])
                 return render(request, 'signup.html', {'error': 'Username has already been taken.'})
             except User.DoesNotExist:
+                # check if valid department
+                # check if valid study program
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password'])
                 auth.login(request, user)
-
+                # create corresponding crc_user table
                 return redirect('dashboard')
         else:
             return render(request, 'signup.html', {'error': 'Passwords must match.'})
     else:
-        return render(request, 'signup.html')
+        user_types = UserType.objects.all
+        return render(request, 'signup.html', {'usertypes': user_types})
 
 def login(request):
     if request.method == "POST":
