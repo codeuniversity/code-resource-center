e3e0b52ecb39fb65874b570c475cde0e7c6cfb98
Daniele de Araujo dos Santos <danisantos@Danieles-MacBook-Pro-2.local>
2018-11-22 08:42:12 +0100

    add function to return names of objects in DB

--- accounts/models.py
+++ accounts/models.py
@@ -7,8 +7,17 @@ from django.db import models
 class UserType(models.Model):
     type_name = models.CharField(max_length=32)
 
+    def __str__(self):
+        return self.type_name
+
 class Institution(models.Model):
     institution_name = models.CharField(max_length=32)
 
+    def __str__(self):
+        return self.institution_name
+
 class Department(models.Model):
     department_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return department_name
