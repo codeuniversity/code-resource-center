1f72a687bf7e1f7780b31b7bcb490429c4181cdd
[Gabriela] <gabriela.sikora@code.berlin>
2018-11-23 13:32:09 +0100

    Refactor to remove raw form and add url validator

--- learningresource/forms.py
+++ learningresource/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .models import LearningResource, MediaType
 from accounts.models import Department
+from django.core.validators import URLValidator
 
 class LearningResourceForm(forms.ModelForm):
+    title       = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Title'}))
+    url         = forms.CharField(validators=[URLValidator()])
+    description = forms.CharField(required=False, widget=forms.Textarea)
+    media_type  = forms.ModelChoiceField(queryset=MediaType.objects.all())
+    department  = forms.ModelChoiceField(queryset=Department.objects.all())
+    is_free     = forms.BooleanField(required=False)
+
     class Meta:
         model = LearningResource
         fields = [
@@ -14,10 +22,11 @@ class LearningResourceForm(forms.ModelForm):
                 'is_free',
         ]
 
-class RawResourceForm(forms.Form):
-    title       = forms.CharField()
-    url         = forms.CharField()
-    description = forms.CharField(required=False, widget=forms.Textarea)
-    media_type  = forms.ModelChoiceField(queryset=MediaType.objects.all())
-    department  = forms.ModelChoiceField(queryset=Department.objects.all())
-    is_free     = forms.BooleanField(required=False)
+
+# class RawResourceForm(forms.Form):
+#     title       = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Title'}))
+#     url         = forms.CharField()
+#     description = forms.CharField(required=False, widget=forms.Textarea)
+#     media_type  = forms.ModelChoiceField(queryset=MediaType.objects.all())
+#     department  = forms.ModelChoiceField(queryset=Department.objects.all())
+#     is_free     = forms.BooleanField(required=False)
