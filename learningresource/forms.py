761445823f62946480127f078413a08cc77bec7b
[Gabriela] <gabriela.sikora@code.berlin>
2018-11-23 13:07:56 +0100

    Implement raw HTML form and pure Django form

--- learningresource/forms.py
+++ learningresource/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import LearningResource
+from .models import LearningResource, MediaType
+from accounts.models import Department
 
 class LearningResourceForm(forms.ModelForm):
     class Meta:
@@ -12,3 +13,11 @@ class LearningResourceForm(forms.ModelForm):
                 'department',
                 'is_free',
         ]
+
+class RawResourceForm(forms.Form):
+    title       = forms.CharField()
+    url         = forms.CharField()
+    description = forms.CharField(required=False, widget=forms.Textarea)
+    media_type  = forms.ModelChoiceField(queryset=MediaType.objects.all())
+    department  = forms.ModelChoiceField(queryset=Department.objects.all())
+    is_free     = forms.BooleanField(required=False)
