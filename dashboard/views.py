6505c4a4dd07c0b0173553eee6e07a1b5b1b8979
Daniele de Araujo dos Santos <danisantos@Danieles-MacBook-Pro-2.local>
2018-11-24 11:16:56 +0100

    Basic implementation of search for description and title. Work in progress

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import auth
-from learningresource.models import LearningResource
 from django.db.models import Q
+from learningresource.models import LearningResource, UserLearningResource
+
 
 @login_required(login_url="/accounts/login")
 def dashboard(request):
     user = User.objects;
+    learningResources = LearningResource.objects.all()
     if user is not None:
-        return render(request, 'dashboard.html')
+        return render(request, 'dashboard.html', {'learningResources': learningResources})
     else:
         return redirect('login')
 
 def searchResult(request):
-    learning_resources = None
     query = None
-    if 'q'in request.GET:
+    if 'q' in request.GET:
         query = request.GET.get('q')
-        learning_resources = LearningResource.objects.all().filter(Q(title__icontains=query)| Q(department__icontains=query))
-        return render(request, 'search.html', {'query': query, 'learningResources': learning_resources})
+        learningResources = LearningResource.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+        return render(request, 'dashboard.html', {'query':query, 'learningResources':learningResources})
+    else:
+        return render(request, 'dashboard.html', {'query':query})
