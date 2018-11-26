2b92c7006062e9c16a321fec05a83398c20755ea
[Gabriela] <gabriela.sikora@code.berlin>
2018-11-26 12:35:04 +0100

    Add tag field and send information to database

--- learningresource/models.py
+++ learningresource/models.py
@@ -30,6 +30,7 @@ class LearningResource(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     votes_total = models.IntegerField(default=1)
     last_edit_date = models.DateTimeField(auto_now_add=True)
+    tag = models.CharField(max_length=255, default='other')
 
     def __str__(self):
         return self.title
