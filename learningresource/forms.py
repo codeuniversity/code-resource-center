f16add7c2b4d1df5ed64885bbd1fe4eaf5fc1ae7
[Gabriela] <gabriela.sikora@code.berlin>
2018-11-30 09:47:04 +0100

    Connect Department field on create page to database with checkbox

--- learningresource/forms.py
+++ learningresource/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import LearningResource, MediaType
 from accounts.models import Department
 from django.core.validators import URLValidator
+from django.forms.widgets import CheckboxSelectMultiple
+
 
 
 class LearningResourceForm(forms.ModelForm):
@@ -30,9 +32,11 @@ class LearningResourceForm(forms.ModelForm):
             queryset=MediaType.objects.all(),
             widget=forms.Select(
                       attrs={
-                              'empty_label':None,
                               'class':'form-control',
                       }))
+                # Note that if a ModelChoiceField is required and has a default initial value, no empty choice is created (regardless of the value of empty_label).
+                # todo 
+
          
 #     department = forms.ModelChoiceField(
 #             queryset=Department.objects.all(),
@@ -64,3 +68,9 @@ class LearningResourceForm(forms.ModelForm):
                 'tag',
         ]
 
+        def __init__(self, *args, **kwargs):
+        
+                super(LearningResourceForm, self).__init__(*args, **kwargs)
+                
+                self.fields["department"].widget = CheckboxSelectMultiple()
+                self.fields["department"].queryset = Department.objects.all()
