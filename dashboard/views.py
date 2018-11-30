c3e36030d7b6804cd9e00c918ca381afabdc4008
Daniele de Araujo dos Santos <danisantos@Danieles-MacBook-Pro-2.local>
2018-11-30 11:19:06 +0100

    trying out code to get author

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from accounts.models import User
 # from django.contrib.auth.models import User
 from django.contrib import auth
+from django.db.models import Count
 from django.db.models import Q
 from learningresource.models import LearningResource, UserLearningResource
 
@@ -21,9 +22,36 @@ def searchResult(request):
     if 'q' in request.GET:
         query = request.GET.get('q')
         learningResources = LearningResource.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+        learning
         return render(request, 'dashboard.html', {'query':query, 'learningResources':learningResources})
     else:
         return render(request, 'dashboard.html', {'query':query})
+
+
+# def getResourceId():
+#     learningResources = LearningResource.objects.all()
+#     for resource in learningResources:
+#         print(resource.id)
+
+def getAuthor(id):
+    #id is hardcoded for now
+    #TODO: find a way to get the resource id from the template!!
+    relations = UserLearningResource.objects.all()
+    for relation in relations:
+        # print(relation.user, relation.learningresource, relation.user.id)
+        if relation.learningresource.id == id:
+            result = UserLearningResource.objects.filter(user=relation.user)
+            author_firstname = relation.user.first_name.capitalize()
+            author_lastname = relation.user.last_name.capitalize()
+            return "{} {}".format(author_firstname, author_lastname)
+
+        else:
+            return "Unidentified Author"
+            # return render(request, 'dashboard.html', {'name': name})
+        #     # name = relation.user.first_name.capitalize() + " " + relation.user.last_name.capitalize()
+        #     return relation.user.id
+        # else:
+        #     return "error"
 
 def filterSoftwareEng(request):
     learningResources = LearningResource.objects.filter(department=1)
